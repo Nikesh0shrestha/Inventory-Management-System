ac8197751bdbfe65a9a7f283d80688d53fd30dcb
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-05-25 13:52:54 +0545

    added product_update and delete views and added urls

--- invApp/views.py
+++ invApp/views.py
@@ -24,12 +24,26 @@ def product_create_view(request):
 #READ VIEW
 def product_list_view(request):
     products = Product.objects.all()
-    return render (request, 'invApp/product_list.html',{'products':'products'})
+    return render (request, 'invApp/product_list.html',{'products':products})
 
 
 #UPDATE VIEW
-
+def product_update_view(request, product_id):
+    product = product.objects.get(product_id = product_id)
+    form = ProductForm(instance=product)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    return redirect(request, 'invApp/product_form.html',{'form': form})    
 
 
 
 #DELETE VIEW
+def product_delete_view(request,product_id):
+    product = Product.objects.get(product_id = product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('product_list')
+    return render(request, 'invApp/product_confirm_delete.html',{'product': product})
