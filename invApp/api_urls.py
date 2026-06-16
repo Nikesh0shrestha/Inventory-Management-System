65e0d2b337b6fc807f4ce5557e74e694b070f334
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-16 16:49:58 +0545

    added ProductDetailAPIView and updated urls

--- invApp/api_urls.py
+++ invApp/api_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .api_views import ProductAPIView
+from .api_views import ProductAPIView, ProductDetailAPIView
 
 urlpatterns = [
     path('products/',ProductAPIView.as_view(),name='api_products'),
+
+    path('products/<int:product_id>/',ProductDetailAPIView.as_view(),name="product_detail")
 ]   
--- invApp/api_views.py
+++ invApp/api_views.py
@@ -1,9 +1,12 @@
+
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import Response, Serializer
 from rest_framework import status
 from.serializers import ProductSerializer
-
 from .models import Product
+
+from django.shortcuts import get_object_or_404
+
 
 class ProductAPIView(APIView):
     def get(self,request):
@@ -13,10 +16,36 @@ class ProductAPIView(APIView):
     
     def post(self,request):
         serializer = ProductSerializer(data=request.data)
-        
+
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetailAPIView(APIView):
+    def get(self,request,product_id):
+        product = get_object_or_404(Product, id=product_id)
+        serializer = ProductSerializer(Product)
+
+        return Response(serializer.data)
+
+    def put(self,request, product_id):
+        product = get_object_or_404(Product,id=product_id)
+        serializer = ProductSerializer(Product,data=request.data)
+
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,product_id):
+        product = get_object_or_404(Product,id=product_id)
+        product.DELETE()
+
+        return Response({"message":"Product deleted successfully!!"},
+        status=status.HTTP_204_NO_CONTENT)
+
