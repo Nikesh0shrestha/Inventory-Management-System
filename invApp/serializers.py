99292e9954039ade78baf7fe49a721510ca5ac08
Nikesh Shrestha <shresthanikesh0777@gmail.com>
2026-06-20 12:20:24 +0545

    Added CategorySerializer and StockTransactionSerialzer

--- invApp/serializers.py
+++ invApp/serializers.py
@@ -1,5 +1,7 @@
+from dataclasses import fields
+from unicodedata import category
 from rest_framework import serializers
-from .models import Product
+from .models import Product,Category,StockTransaction
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,3 +11,19 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
 
         fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+
+        model = category
+
+        fields = '__all__'
+
+class StockTransactionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        
+        model = StockTransaction
+
+        fields = "__all__"
